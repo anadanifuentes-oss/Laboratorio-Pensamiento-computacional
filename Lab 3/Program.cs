8d912f81d1a2c8fbb9d24f575a8d1e9978f9a485
anadanifuentes-oss <anadani.fuentes@gmail.com>
2026-02-06 23:09:33 -0600

    Add files via upload

---
+++ Lab 3/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Lab03_A2_Ana_Fuentes
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            // Solicitar datos
+            Console.WriteLine("Ingrese el nombre del estudiante:");
+            string? nombreEstudiante = Console.ReadLine();
+
+            Console.WriteLine("Ingrese el nombre del curso:");
+            string? nombreCurso = Console.ReadLine();
+
+            // Mostrar los datos
+            Console.WriteLine();
+            Console.WriteLine(nombreCurso);
+            Console.WriteLine(
+                (nombreEstudiante ?? "Estudiante") +
+                ", espero que te esté gustando el curso de " +
+                (nombreCurso ?? "el curso") +
+                " tanto como a mí."
+            );
+
+            Console.WriteLine("Nunca dejes de practicar, ya que veo que vas por buen camino.");
+            Console.WriteLine("Ya puedes continuar con tu día. Adiós");
+
+            // Finalizar programa
+            Console.WriteLine("\nPresione una tecla para salir...");
+            Console.ReadKey();
+        }
+    }
+}
