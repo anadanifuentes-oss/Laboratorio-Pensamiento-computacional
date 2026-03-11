28923024ee275b7efe5475d5c771371cfde51f4a
anadanifuentes-oss <anadani.fuentes@gmail.com>
2026-03-11 08:35:10 -0600

    Add files via upload

---
+++ Lab 8/Lab08-A1-A5-Ana Fuentes/Lab08-A1-A5-Ana Fuentes/Program.cs
@@ -0,0 +1,165 @@
+﻿using System;
+
+namespace Lab08_A1_A5_Ana_Fuentes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ejercicio 1: Solicitar 20 números al usuario
+            Console.WriteLine("Ejercicio 1: Solicitar 20 números al usuario");
+
+            int numero;
+            int mayor = 0;
+            int menor = int.MaxValue;
+            int suma = 0;
+
+            for (int i = 1; i <= 20; i++)
+            {
+                Console.Write("Ingrese número " + i + ": ");
+                numero = Convert.ToInt32(Console.ReadLine());
+
+                if (numero > mayor)
+                {
+                    mayor = numero;
+                }
+
+                if (numero < menor)
+                {
+                    menor = numero;
+                }
+
+                suma = suma + numero;
+            }
+
+            double promedio = (double)suma / 20;
+            Console.WriteLine();
+
+            Console.WriteLine("Número mayor: " + mayor);
+            Console.WriteLine("Número menor: " + menor);
+            Console.WriteLine("Promedio: " + promedio);
+            Console.WriteLine();
+
+            // Ejercicio 2: Mostrar números del 1 al 100 con condiciones
+            Console.WriteLine("Ejercicio 2: Mostrar números del 1 al 100 con condiciones");
+
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 2 == 0 && i % 7 == 0)
+                {
+                    Console.WriteLine("ParSiete");
+                }
+                else if (i % 2 == 0)
+                {
+                    Console.WriteLine("Par");
+                }
+                else if (i % 7 == 0)
+                {
+                    Console.WriteLine("Siete");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.WriteLine();
+
+            // Ejercicio 3: Calcular descuentos para clientes
+            Console.WriteLine("Ejercicio 3: Calcular descuentos para clientes");
+
+            double compra;
+            double totalDia = 0;
+            int clientesDescuento = 0;
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.Write("Ingrese monto de compra del cliente " + i + ": ");
+                compra = Convert.ToDouble(Console.ReadLine());
+
+                double descuento = 0;
+
+                if (compra > 700)
+                {
+                    descuento = compra * 0.12;
+                    clientesDescuento++;
+                }
+                else if (compra > 300)
+                {
+                    descuento = compra * 0.05;
+                    clientesDescuento++;
+                }
+
+                double totalPagar = compra - descuento;
+
+                Console.WriteLine("Total pagado por cliente " + i + ": " + totalPagar);
+
+                totalDia = totalDia + totalPagar;
+            }
+            Console.WriteLine();
+            Console.WriteLine("Clientes con descuento: " + clientesDescuento);
+            Console.WriteLine("Total de ventas del día: " + totalDia);
+            Console.WriteLine();
+
+            // Ejercicio 4: Menú de opciones para mostrar números
+            Console.WriteLine("Ejercicio 4: Menú de opciones para mostrar números");
+
+            Console.Write("Ingrese un número entero: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("1. Mostrar los números desde el número ingresado hasta 1");
+            Console.WriteLine("2. Mostrar los múltiplos de 3 hasta el número ingresado");
+            Console.WriteLine("3. Mostrar los múltiplos de 5 hasta el número ingresado");
+
+            Console.Write("Seleccione una opción: ");
+            int opcion = Convert.ToInt32(Console.ReadLine());
+
+            if (opcion == 1)
+            {
+                for (int i = n; i >= 1; i--)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            else if (opcion == 2)
+            {
+                for (int i = 1; i <= n; i++)
+                {
+                    if (i % 3 == 0)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+            }
+            else if (opcion == 3)
+            {
+                for (int i = 1; i <= n; i++)
+                {
+                    if (i % 5 == 0)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Opción no válida");
+            }
+            Console.WriteLine();
+
+            // Ejercicio 5: Mostrar triángulo de asteriscos
+            Console.WriteLine("Ejercicio 5: Mostrar triángulo de asteriscos");
+
+            Console.Write("Ingrese número de filas: ");
+            int filas = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= filas; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Lab 8/Lab08-A1-A5-Ana Fuentes/Lab08-A1-A5-Ana Fuentes/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("Lab08-A1-A5-Ana Fuentes")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Lab08-A1-A5-Ana Fuentes")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("bf44a429-eb9f-4058-b272-e58b09ef1fc1")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
